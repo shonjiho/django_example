5d78978fafb580e9bacabd227bfaa08b95c7797e
shonjiho <jihoson94@email.com>
2019-09-21 13:52:06 +0000

    add polls/model.Question was_published_recently test case

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
@@ -9,7 +10,8 @@ class Question(models.Model):
 		return self.question_text
 	
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		date_now = timezone.now()
+		return date_now >= self.pub_date >= date_now - datetime.timedelta(days=1)
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -18,3 +20,4 @@ class Choice(models.Model):
 
 	def __str__(self):
 		return self.choice_text
+
