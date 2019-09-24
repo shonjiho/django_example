f2b0f528bf96ebd992d028260393f8568ab8c29b
shonjiho <jihoson94@email.com>
2019-09-24 06:48:18 +0000

    Improving View, Testing DetailView and IndexView

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice 
 
@@ -11,11 +12,14 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 	
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/question_detail.html'
+	
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
 	
 class ResultsView(generic.DetailView):
 	model = Question
@@ -26,7 +30,7 @@ def vote(request, question_id):
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/detail.html', {
+		return render(request, 'polls/question_detail.html', {
 			'question': question,
 			'error_message': "You didn`t select a choice",
 		})
